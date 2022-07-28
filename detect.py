e42959ebb89cfe19a89e35ea75e233f5e54cdada
Syed Mukarramuddin <nasrite@gmail.com>
2022-07-28 23:52:58 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_facefrontal.xml')
 
 
-img = cv2.imread('degeners.jpg')
+img = cv2.imread('path/to/Img.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
